7a48ff888b5addef021371711521da82517409d8
NarutSH <narut.sh@gmail.com>
2023-02-14 09:51:44 +0700

    update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const position = [13.0, 101.0];
+
+  return (
+    <div>
+      <MapContainer
+        center={position}
+        zoom={10}
+        style={{ width: "100%", height: "400px" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            <img
+              src="https://images.unsplash.com/photo-1578769242615-0516cbd406f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1009&q=80"
+              alt="office"
+              width={100}
+            />
+            <p>My Building</p>
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Contact;
